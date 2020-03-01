a5d5525fc3616e295751f2e5c84ef799127524ca
Shane Celis <shane.celis@gmail.com>
2020-03-01 05:26:45 -0500

    test: Add tests for frequency classes.

--- test/FrequencyCountTests.cs
+++ test/FrequencyCountTests.cs
@@ -14,6 +14,15 @@ public class FrequencyCountTests {
   }
 
   [Fact]
+  public void TestOneSampleArray() {
+    var fc = new FrequencyArraySingle(10, -1f, 1f);
+    fc.Add(new [] { 0f });
+    Assert.Equal(new [] { 1f }, fc.Probability(new [] { 0f }));
+    Assert.Equal(new [] { 0f }, fc.Probability(new [] { 1f }));
+    Assert.Equal(new [] { 0f }, fc.Entropy());
+  }
+
+  [Fact]
   public void TestTwoSamples() {
     var fc = new FrequencyCountSingle(10, -1f, 1f);
     fc.Add(0f);
@@ -40,6 +49,70 @@ public class FrequencyCountTests {
     fc.Add("a");
     fc.Add("b");
     Assert.Throws<ArgumentException>(() => fc.Add("c"));
+  }
+
+
+  [Fact]
+  public void TestPairedAlphabet() {
+    var fc = new FrequencyPairAlphabet(new[] { "a", "b" });
+    fc.Add("a", 0);
+    fc.Add("a", 0);
+    fc.Add("a", 1);
+    fc.Add("a", 1);
+    fc.Add("b", 0);
+    fc.Add("b", 0);
+    fc.Add("b", 0);
+    fc.Add("b", 0);
+    fc.Add("b", 1);
+    Assert.Equal(4f/9, fc.ProbabilityX("a"));
+    Assert.Equal(5f/9, fc.ProbabilityX("b"));
+    Assert.Equal(6/9f, fc.ProbabilityY(0));
+    Assert.Equal(3/9f, fc.ProbabilityY(1));
+    Assert.Equal(2/9f, fc.ProbabilityXY("a", 0));
+    Assert.Equal(2/4f, fc.ProbabilityYGivenX(0, "a"));
+    Assert.Equal(2/4f, fc.ProbabilityYGivenX(1, "a"));
+    Assert.Equal(4/5f, fc.ProbabilityYGivenX(0, "b"), 2);
+    Assert.Equal(1/5f, fc.ProbabilityYGivenX(1, "b"), 2);
+    Assert.Equal(0.8f, fc.EntropyYGivenX(), 1);
+    Assert.Equal(0.9f, fc.EntropyXGivenY(), 1);
+    Assert.Equal(1.8f, fc.EntropyXY(), 1);
+    Assert.Equal(0.99f, fc.EntropyX(), 2);
+    Assert.Equal(0.9f, fc.EntropyY(), 1);
+    Assert.Equal(0f, fc.EntropyXGivenY() - fc.EntropyXY() + fc.EntropyY(), 1);
+    Assert.Equal(0f, fc.EntropyYGivenX() - fc.EntropyXY() + fc.EntropyX(), 1);
+    Assert.Equal(0.07f, fc.MutualInformationXY(), 2);
+    // Assert.Equal(1f, fc.Entropy(), 2);
+  }
+
+  [Fact]
+  public void TestPairedAlphabet2() {
+    var fc = new FrequencyPairAlphabet(new[] { "a", "b" });
+    fc.Add("a", 1);
+    fc.Add("a", 1);
+    fc.Add("a", 1);
+    fc.Add("a", 1);
+    fc.Add("b", 0);
+    fc.Add("b", 0);
+    fc.Add("b", 0);
+    fc.Add("b", 0);
+    Assert.Equal(4/8f, fc.ProbabilityX("a"));
+    Assert.Equal(4/8f, fc.ProbabilityX("b"));
+    Assert.Equal(4/8f, fc.ProbabilityY(0));
+    Assert.Equal(4/8f, fc.ProbabilityY(1));
+    Assert.Equal(0f, fc.ProbabilityXY("a", 0));
+    Assert.Equal(0f, fc.ProbabilityYGivenX(0, "a"));
+    Assert.Equal(1f, fc.ProbabilityYGivenX(1, "a"));
+    Assert.Equal(1f, fc.ProbabilityYGivenX(0, "b"), 2);
+    Assert.Equal(0f, fc.ProbabilityYGivenX(1, "b"), 2);
+    Assert.Equal(0f, fc.EntropyYGivenX(), 1);
+    Assert.Equal(0f, fc.EntropyXGivenY(), 1);
+    Assert.Equal(1f, fc.EntropyXY(), 1);
+    Assert.Equal(1f, fc.EntropyX(), 2);
+    Assert.Equal(1f, fc.EntropyY(), 1);
+    Assert.Equal(0f, fc.EntropyXGivenY() - fc.EntropyXY() + fc.EntropyY(), 1);
+    Assert.Equal(0f, fc.EntropyYGivenX() - fc.EntropyXY() + fc.EntropyX(), 1);
+    Assert.Equal(1f, fc.MutualInformationXY(), 2);
+    // Assert.Equal(1f, fc.Entropy(), 2);
   }
 }
 }
